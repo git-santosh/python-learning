ff2181bf78802b533d531ff51be91da3f5b86304
Santosh Suryawanshi <santosh.suryawanshi28@gmail.com>
2019-01-19 13:45:41 +0530

    some more statement to test

--- helloWorld/helloworld.py
+++ helloWorld/helloworld.py
@@ -1,2 +1,10 @@
 msg = "Hello world"
 print(msg)
+print(10+21)
+print(9*8)
+print()
+print('the end')
+print('santosh is vary "good" at progrramming')
+greeting = "good afternoon"
+name = "everybody"
+print(greeting + ' ' + name)
