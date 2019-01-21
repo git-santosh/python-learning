0369acdf8bf4fde1a03ea50a031c868f7034c807
Santosh Suryawanshi <santosh.suryawanshi28@gmail.com>
2019-01-21 17:08:53 +0530

    control characters in python

---
+++ control characters/char_print.py
@@ -0,0 +1,22 @@
+# control characters
+# back slash 
+# new line
+# tab 
+
+splitString = "santosh \nmadhukar \nsuryawanshi"
+print(splitString)
+
+tabbed = "1\t2\t3\t4\t5"
+print(tabbed)
+#embded slashes 
+print('Coffee shop owner said "No, no \'e\'s uh,...We are closed"')
+print("Coffee shop owner said \"No, no 'e's uh,...We are closed\"")
+
+# triple quotes to avoid backslashes 
+# equivalent function of above
+anotherSplitString = """this string has been 
+split over 
+several lines """
+print(anotherSplitString)
+print()
+print("""Coffee shop owner said "No, no 'e's uh,...We are closed" """)
---
+++ input/input.py
@@ -0,0 +1,8 @@
+greeting = "Hello"
+name = input('Enter your Name ?')
+
+print(greeting + ' ' + name )
+
+porgrmaingLang = input('May I know what progrmming language you like most ?')
+
+print('glad to hear that you also like '+ porgrmaingLang)
