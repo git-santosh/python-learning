f40029c30a30461a5596f6db4a6aec48d434a3ca
Santosh Suryawanshi <santosh.suryawanshi28@gmail.com>
2019-01-23 17:05:52 +0530

    multiple assignments

--- variables_dataType/store_value.py
+++ variables_dataType/store_value.py
@@ -2,6 +2,9 @@ greeting = "hello"
 _myName = "santosh"
 age = 28
 
+n1 , n2 , n3 = "p1","p2","p3"
 print(greeting + ' '+_myName)
 
-print('My age is '+age)
+#print('My age is '+age)
+
+print(n1+' '+ n2+' '+n3)
